ab5fcbeeeb5552b43f3a424ff16a5a2bd0cecb3f
AilcoGeveke <ailcoharmen@live.nl>
2018-06-14 13:40:06 +0200

    dinge

---
+++ Direct_Drive_Program/Direct_Drive_Program.ino
@@ -0,0 +1,32 @@
+const double motorConstante = 0.5424;
+const double mechanischeConstante = 0.222;
+
+double pi = 3.14.....;
+double hoekVersnelling;
+double hoeksnelheid;
+double koppelTrapperL;
+double koppelTrapperR;
+double koppelFietser;
+double koppelMotor;
+double koppelNetto;
+double massatraagheid;
+double klemspanning;
+double armLengte;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void readValues();
+void calculateHoek();
+void calculateKoppel();
+void calculateMassatraagheid();
+void calculateKlemspanning();
+
