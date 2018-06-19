12e01cdaf623b03512dd7adba6499ff375bec0de
AilcoGeveke <ailcoharmen@live.nl>
2018-06-19 15:50:05 +0200

    Hoeksnelheid

--- Trapkracht/Trapkracht.ino
+++ Trapkracht/Trapkracht.ino
@@ -1,11 +1,16 @@
-int fsrPin1 = 0;   
-int fsrPin2 = 1;  
+#include <elapsedMillis.h>
+
+elapsedMillis timeElapsed;
+int fsrPin1 = 0;
+int fsrPin2 = 1;
 int DrukSensor1;
 int DrukSensor2;
 float Voltage1;
 float Voltage2;
 float fsrResistance;
 float fsrConductance;
+float nettoTijd, vorigeTijd = 0;
+float f, w, a = 0; //f=freq,w=hoeksnelheid,a=hoekversnelling
 
 int encoderPinA = 3;
 int encoderPinB = 4;
@@ -25,10 +30,10 @@ void setup() {
 }
 
 void loop() {
-  traphoek();  
+  traphoek();
 }
 
-void traphoek(){
+void traphoek() {
   n = digitalRead(encoderPinA);
   if ((encoderPinALast == LOW) && (n == HIGH)) {
     if (digitalRead(encoderPinB) == HIGH) {
@@ -36,10 +41,11 @@ void traphoek(){
     } else {
       encoderPos = encoderPos + stap;
     }
+    hoeksnelheid();
     DrukSensor1 = analogRead(fsrPin1);
     DrukSensor2 = analogRead(fsrPin2);
     arbeid();
-    if(encoderPos > 1.99){
+    if (encoderPos > 1.99) {
       encoderPos = 0;
     }
   }
@@ -50,7 +56,7 @@ void traphoek(){
 void arbeid() {
   Voltage1 = map(DrukSensor1, 0, 1023, 0, 5000);
   Voltage2 = map(DrukSensor2, 0, 1023, 0, 5000);
-  fsrResistance = 5000 - Voltage1; 
+  fsrResistance = 5000 - Voltage1;
   fsrResistance *= 10000;
   fsrResistance /= Voltage1;
   fsrConductance = 1000000;
@@ -61,7 +67,7 @@ void arbeid() {
     N1 = fsrConductance - 1000;
     N1 /= 30;
   }
-  fsrResistance = 5000 - Voltage2; 
+  fsrResistance = 5000 - Voltage2;
   fsrResistance *= 10000;
   fsrResistance /= Voltage2;
   fsrConductance = 1000000;
@@ -72,11 +78,25 @@ void arbeid() {
     N2 = fsrConductance - 1000;
     N2 /= 30;
   }
-  W = N1*arm*cos(encoderPos*PI) + N2*arm*cos((1 + encoderPos)*PI); 
+  W = N1 * arm * cos(encoderPos * PI) + N2 * arm * cos((1 + encoderPos) * PI);
   Serial.print(W);
   Serial.println("Nm");
   //Serial.print("N1: ");
   //Serial.println(N1);
   //Serial.print("N2: ");
   //Serial.println(N2);
+}
+
+void hoeksnelheid()
+{
+  //w = 2 pi f
+  //f = 1 / T
+
+  nettoTijd = timeElapsed - vorigeTijd;
+  vorigeTijd = timeElapsed;
+
+  f = 1 / (12 * (nettoTijd));
+  w = 2 * PI * f;
+
+  //hoekversnelling(a) = W - vorigeW / nettoTijd 
 }
