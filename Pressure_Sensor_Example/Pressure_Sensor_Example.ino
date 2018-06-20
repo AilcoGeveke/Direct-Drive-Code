5797f38bcc4fbb149ed3bc8198ff05c741ab6f93
NickStaal <39586041+NickStaal@users.noreply.github.com>
2018-06-20 14:08:42 +0200

    Curve FSR bepaling

--- Pressure_Sensor_Example/Pressure_Sensor_Example.ino
+++ Pressure_Sensor_Example/Pressure_Sensor_Example.ino
@@ -4,7 +4,7 @@ Connect one end of FSR to power, the other end to Analog 0.
 Then connect one end of a 10K resistor from Analog 0 to ground 
  
 For more information see www.ladyada.net/learn/sensors/fsr.html */
- 
+float Vout = 0; 
 int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
 int fsrReading;     // the analog reading from the FSR resistor divider
  
@@ -15,21 +15,8 @@ void setup(void) {
  
 void loop(void) {
   fsrReading = analogRead(fsrPin);  
- 
+  Vout = map(fsrReading, 0, 1023, 0, 5000);
   Serial.print("Analog reading = ");
-  Serial.print(fsrReading);     // the raw analog reading
- 
-  // We'll have a few threshholds, qualitatively determined
-  if (fsrReading < 10) {
-    Serial.println(" - No pressure");
-  } else if (fsrReading < 200) {
-    Serial.println(" - Light touch");
-  } else if (fsrReading < 500) {
-    Serial.println(" - Light squeeze");
-  } else if (fsrReading < 800) {
-    Serial.println(" - Medium squeeze");
-  } else {
-    Serial.println(" - Big squeeze");
-  }
+  Serial.println(Vout);     // the raw analog reading
   delay(1000);
 } 
