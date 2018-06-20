9ecdc87f8e9b2c3f77fa3f2b1e5d5f68ae40fe35
AilcoGeveke <ailcoharmen@live.nl>
2018-06-20 13:33:34 +0200

    Hoeksnelheid + Ijken Druksensor

--- Trapkracht/Trapkracht.ino
+++ Trapkracht/Trapkracht.ino
@@ -10,7 +10,7 @@ float Voltage2;
 float fsrResistance;
 float fsrConductance;
 float nettoTijd, vorigeTijd = 0;
-float f, w, a = 0; //f=freq,w=hoeksnelheid,a=hoekversnelling
+float f, w, vorigeW, a = 0; //f=freq,w=hoeksnelheid,a=hoekversnelling
 
 int encoderPinA = 3;
 int encoderPinB = 4;
@@ -33,6 +33,7 @@ void loop() {
   traphoek();
 }
 
+
 void traphoek() {
   n = digitalRead(encoderPinA);
   if ((encoderPinALast == LOW) && (n == HIGH)) {
@@ -41,10 +42,12 @@ void traphoek() {
     } else {
       encoderPos = encoderPos + stap;
     }
-    hoeksnelheid();
-    DrukSensor1 = analogRead(fsrPin1);
-    DrukSensor2 = analogRead(fsrPin2);
-    arbeid();
+    hoeksnelheid();                     //Bereken hoeksnelheid en hoekversnelling
+    DrukSensor1 = analogRead(fsrPin1);  //Uitlezen van Druksensors
+    Serial.print("DrukSensor1:");
+    Serial.println(DrukSensor1);
+    DrukSensor2 = analogRead(fsrPin2);  //
+    arbeid();                           //Arbeid op trappers berekenen
     if (encoderPos > 1.99) {
       encoderPos = 0;
     }
@@ -54,30 +57,41 @@ void traphoek() {
 }
 
 void arbeid() {
-  Voltage1 = map(DrukSensor1, 0, 1023, 0, 5000);
-  Voltage2 = map(DrukSensor2, 0, 1023, 0, 5000);
-  fsrResistance = 5000 - Voltage1;
+  Voltage1 = map(DrukSensor1, 0, 1023, 0, 5000);      //Sensorwaardes converteren naar milliVolts
+  Serial.print("Voltage1:");
+  Serial.println(Voltage1);
+  Voltage2 = map(DrukSensor2, 0, 1023, 0, 5000);      //
+
+  //Formule y = 34331050 + (2.047047 - 34331050)/(1 + (x/162446.1)^3.412111)
+  fsrResistance = 5000 - Voltage1;                    
   fsrResistance *= 10000;
   fsrResistance /= Voltage1;
   fsrConductance = 1000000;
   fsrConductance /= fsrResistance;
+
+//If-Else loop om kracht op eerste trapper te berekenen
   if (fsrConductance <= 1000) {
     N1 = fsrConductance / 80;
   } else {
     N1 = fsrConductance - 1000;
     N1 /= 30;
   }
+  Serial.print("N1:");
+  Serial.println(N1);
   fsrResistance = 5000 - Voltage2;
   fsrResistance *= 10000;
   fsrResistance /= Voltage2;
   fsrConductance = 1000000;
   fsrConductance /= fsrResistance;
+
+  //If-Else loop om kracht op tweede trapper te berekenen
   if (fsrConductance <= 1000) {
     N2 = fsrConductance / 80;
   } else {
     N2 = fsrConductance - 1000;
     N2 /= 30;
   }
+  
   W = N1 * arm * cos(encoderPos * PI) + N2 * arm * cos((1 + encoderPos) * PI);
   Serial.print(W);
   Serial.println("Nm");
@@ -89,14 +103,19 @@ void arbeid() {
 
 void hoeksnelheid()
 {
+  //--- Formules ---
   //w = 2 pi f
   //f = 1 / T
+  //----------------
 
-  nettoTijd = timeElapsed - vorigeTijd;
-  vorigeTijd = timeElapsed;
+  nettoTijd = (float)timeElapsed - vorigeTijd;
+  vorigeTijd = (float)timeElapsed;
 
   f = 1 / (12 * (nettoTijd));
   w = 2 * PI * f;
 
-  //hoekversnelling(a) = W - vorigeW / nettoTijd 
+  a = w - vorigeW / nettoTijd;
+  vorigeW = w;
+
+  
 }
