84a5b3002b00e41d7a3ef926c503fe7739482124
AilcoGeveke <ailcoharmen@live.nl>
2018-06-21 17:27:53 +0200

    Klemspanning works kinda

--- Direct_Drive_Program/Direct_Drive_Program.ino
+++ Direct_Drive_Program/Direct_Drive_Program.ino
@@ -6,28 +6,33 @@ const int fsrPin2 = 3;                        //Analoog
 const int resetPin = 4;                       // select the input pin for the interrupter
 const int encoderPinA = 5;                      //Digitaal
 const int encoderPinB = 4;                      //Digitaal
-const int vermogenMotor = 40;
-const float motorConstante = 0.5424;             //Kw of Km
-const float mechanischeConstante = 0.222;        //Ke
+const float vermogenMotor = 0.5;
+const float motorConstante = 0.43;             //Kw of Km
+const float mechanischeConstante = 3;        //Ke
+const float Jfiets = 0.001;
+const float Jmotor = 0.0001433;        //Jmotor
+float gewensteA = 0;
 
-int countSteps = 0;
+int countRes, countSteps = 0;
 int DrukSensor1, DrukSensor2;
 float Voltage1, Voltage2;
-float nettoTijd, vorigeTijd = 0;
-float f, w, vorigeW, a = 0; //f=freq,w=hoeksnelheid,a=hoekversnelling
+float tijd, nettoTijd, vorigeTijd, totaalTijd = 0;
+float f, w, vorigeW, w2, a = 0; //f=freq,w=hoeksnelheid,a=hoekversnelling
 
 
-float encoderPos = -0.08333;
+float encoderPos = -0.5;
 int encoderPinALast = LOW;
 int n = LOW;
 int motorSnelheid = 0;
 int resetWaarde;
+int deler = 0;
 float arm = 0.10;
 float stap = 0.08333;
+float fsrResistance, fsrConductance;
 float N1 = 0;
 float N2 = 0;
-float W = 0;
-float massatraagheid;         //I
+float W, Wt = 0;
+
 float klemspanning;           //Uk
 float motorKoppel;
 float nettoKoppel;
@@ -36,8 +41,8 @@ const int ankerweerstand = 89;         //Ra
 
 void setup() {
   //Setup Motor Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT); //Initiates Brake Channel B pin
+  pinMode(12, OUTPUT); //Initiates Motor Channel B pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel B pin
 
   pinMode (encoderPinA, INPUT_PULLUP);
   pinMode (encoderPinB, INPUT_PULLUP);
@@ -52,108 +57,114 @@ void loop() {
 void mainProgram() {
   n = digitalRead(encoderPinA);
   resetWaarde = analogRead(resetPin);
-  if (resetWaarde < 50 || encoderPos > 1.99) {
-    encoderPos = 0;
+  if (resetWaarde < 50 || encoderPos > 1.49) {
+    encoderPos = -0.5;
     countSteps = 0;
+    totaalTijd = 0;
+    w2 = 0;
   }
   if ((encoderPinALast == LOW) && (n == HIGH)) {
     if (digitalRead(encoderPinB) == HIGH) {
       encoderPos = encoderPos + stap;
       countSteps++;
+      Serial.println("------------------------------------------");
+      hoeksnelheid();                     //Bereken hoeksnelheid en hoekversnelling
+      arbeid();                           //Arbeid op trappers berekenen
+      //berekenKlemspanning();
+      //Serial.println("------------------------------------------");
     } else {
       //encoderPos = encoderPos - stap;
     }
-    //Serial.println("------------------------------------------");
-    hoeksnelheid();                     //Bereken hoeksnelheid en hoekversnelling
-    DrukSensor1 = analogRead(fsrPin1);  //Uitlezen van Druksensors
-    //Serial.print("DrukSensor1:");
-    //Serial.println(DrukSensor1);
-    DrukSensor2 = analogRead(fsrPin2);  //
-    //Serial.print("DrukSensor2:");
-    //Serial.println(DrukSensor2);
-    arbeid();                           //Arbeid op trappers berekenen
-    //berekenNettoKracht();
-    //berekenKlemspanning();
-    //Serial.println("------------------------------------------");
   }
   encoderPinALast = n;
 
 }
 
 void arbeid() {
+  DrukSensor1 = analogRead(fsrPin1);  //Uitlezen van Druksensors
+  DrukSensor2 = analogRead(fsrPin2);  //
   Voltage1 = map(DrukSensor1, 0, 1023, 0, 5000);      //Sensorwaardes converteren naar milliVolts
-  //Serial.print("Voltage1:");
-  //Serial.println(Voltage1);
-  //Serial.print("Voltage2:");
-  //Serial.println(Voltage2);
-  Voltage2 = map(DrukSensor2, 0, 1023, 0, 5000);      //
+  Voltage2 = map(DrukSensor2, 0, 1023, 0, 5000);
+  fsrResistance = 5000 - Voltage1;
+  fsrResistance *= 10000;
+  fsrResistance /= Voltage1;
+  fsrConductance = 1000000;
+  fsrConductance /= fsrResistance;
+  if (fsrConductance <= 1000) {
+    N1 = fsrConductance / 80;
+  } else {
+    N1 = fsrConductance - 1000;
+    N1 /= 30;
+  }
 
-  //Formule y = 34331050 + (2.047047 - 34331050)/(1 + (x/162446.1)^3.412111)
-  if (Voltage1 != 0)
-    N1 = 34331050 + (2.047047 - 34331050) / (1 + (pow((Voltage1 / 162446.1), 3.412111)));
-  else
-    N1 = 0;
-  //Serial.print("NewtonForce1:");
-  //Serial.print(N1);
-  //Serial.println(" N");
+  fsrResistance = 5000 - Voltage2;
+  fsrResistance *= 10000;
+  fsrResistance /= Voltage2;
+  fsrConductance = 1000000;
+  fsrConductance /= fsrResistance;
 
-  if (Voltage2 != 0)
-    N2 = 34331050 + (2.047047 - 34331050) / (1 + (pow((Voltage2 / 162446.1), 3.412111)));
-  else
-    N2 = 0;
-  //Serial.print("NewtonForce2:");
-  //Serial.print(N2);
-  //Serial.println(" N");
+  //If-Else loop om kracht op tweede trapper te berekenen
+  if (fsrConductance <= 1000) {
+    N2 = fsrConductance / 80;
+  } else {
+    N2 = fsrConductance - 1000;
+    N2 /= 30;
+  }
 
-  //Serial.print("Arm:");
-  //Serial.print(arm);
-  //Serial.println(" m");
+  //Serial.print("Counter:");
+  //Serial.println(countSteps);
 
-  Serial.print("EncoderPositie:");
-  Serial.println(encoderPos);
-  Serial.print("Counter:");
-  Serial.println(countSteps);
-  //Serial.println(" rad");
+
   W = N1 * arm * cos(encoderPos * PI) + N2 * arm * cos((1 + encoderPos) * PI);
-  //Serial.print("Arbeid:");
-  //Serial.print(W);
-  //Serial.println("Nm");
+  Wt = Wt + w;
+  deler++;
+  if (countSteps == 1) {
+    Wt = Wt / deler;
+    nettoKracht();
+    //Serial.print("Arbeid:");
+    //Serial.print(Wt);
+    //Serial.println("Nm");
+    Wt = 0;
+    deler = 0;
+  }
+
 }
+
 
 void hoeksnelheid()
 {
-  //--- Formules ---
-  //w = 2 pi f
-  //f = 1 / T
-  //----------------
+  if (countSteps == 6 || countSteps == 12 || countSteps == 18 || countSteps == 24 ) {
+    tijd = (float)timeElapsed / 1000;     //Delen door duizend: ms => s
+    nettoTijd = (tijd - vorigeTijd);
+    vorigeTijd = tijd;
 
-  nettoTijd = ((float)timeElapsed - vorigeTijd) / 1000;       //Delen door duizend: ms => s
-  vorigeTijd = (float)timeElapsed;
+    f = 1 / (4 * (nettoTijd));
+    w = 2 * PI * f;
+    a = (w - vorigeW) / nettoTijd;
+    vorigeW = w;
 
-  f = 1 / (12 * (nettoTijd));
-  w = 2 * PI * f;
-
-  a = w - vorigeW / nettoTijd;
-  vorigeW = w;
+    //Serial.print("Hoeksnelheid:");
+    //Serial.print(w);
+    //Serial.println(" rad/s");
+    //Serial.print("Hoekversnelling:");
+    //Serial.print(a);
+    //Serial.println(" rad/s^2");
+  }
 }
 
 
-void berekenNettoKracht() //T = Iα
+void nettoKracht() //T = Iα
 {
-  if (a == 0)
-    massatraagheid = 0;
-  else
-    massatraagheid = W / a;                               //I = Tlast / α
 
-  motorKoppel = motorConstante * massatraagheid;        //Tm = Kw * I
-  nettoKoppel = motorKoppel - W;                        //Tnetto = Tm - Tlast
-
-  Serial.print("Massatraagheid:");
-  Serial.print(massatraagheid);
-  Serial.println(" ??\t");
-  Serial.print(" MotorKoppel:");
-  Serial.print(motorKoppel);
-  Serial.println(" Nm\t");
+  nettoKoppel = W - vermogenMotor / w; //Tn = Tlast - Tstatisch
+  gewensteA = nettoKoppel / Jfiets;
+  berekenKlemspanning();
+  //Serial.print("Massatraagheid:");
+  //Serial.print(massatraagheid);
+  //Serial.println(" ??\t");
+  //Serial.print(" MotorKoppel:");
+  //Serial.print(motorKoppel);
+  //Serial.println(" Nm\t");
   Serial.print(" NettoKoppel:");
   Serial.print(nettoKoppel);
   Serial.println(" Nm");
@@ -161,11 +172,13 @@ void berekenNettoKracht() //T = Iα
 
 void berekenKlemspanning() {
   //Uk = 1000 * (Pm * (Ra / (Km * w) + Ke * w));       //Klemspanning in milliVolts dus * 1000
-  if (massatraagheid == 0)
-    klemspanning = 1000 * (vermogenMotor * (ankerweerstand / (motorConstante * w) + mechanischeConstante * w));
+  if (a > -0.1 || a < 0.1)
+    klemspanning =  (vermogenMotor * ((ankerweerstand) / (motorConstante * w)) + (mechanischeConstante * w));
   else
-    klemspanning = 1000 * ((ankerweerstand / motorConstante) * (nettoKoppel + (vermogenMotor)) + mechanischeConstante * w);
+    klemspanning =  ((ankerweerstand / motorConstante) * (gewensteA * Jmotor + vermogenMotor / w) + mechanischeConstante * w);
 
+  if (w < 0.6)
+    klemspanning = 0;
   Serial.print("Klemspanning:");
   Serial.print(klemspanning);
   Serial.println(" mV");
@@ -173,20 +186,26 @@ void berekenKlemspanning() {
   if (klemspanning < 0)
   {
     klemspanning = klemspanning * -1;
-    motorSnelheid = map(klemspanning, 0, 12000, 0, 255);
-    digitalWrite(13, LOW); //Establishes backward direction of Channel B
-    digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-    analogWrite(11, motorSnelheid);   //Spins the motor on Channel B at given speed
-    delay(10);                  //-----------------------DELAY?????????????-----------------
-    digitalWrite(8, HIGH);   //Engage the Brake for Channel B
+    motorSnelheid = map(klemspanning, 0, 120, 0, 90);
+    Serial.print("MotorSnelheid 0-255:");
+    Serial.println(motorSnelheid);
+    if (motorSnelheid > 40)
+      motorSnelheid = 40;
+    digitalWrite(12, HIGH); //Establishes backward direction of Channel B
+    digitalWrite(9, LOW);   //Disengage the Brake for Channel B
+    analogWrite(3, motorSnelheid);   //Spins the motor on Channel B at given speed
+    //digitalWrite(, HIGH);   //Engage the Brake for Channel B
   }
   else
   {
-    motorSnelheid = map(klemspanning, 0, 12000, 0, 255);
-    digitalWrite(13, HIGH);           //Establishes forward direction of Channel B
-    digitalWrite(8, LOW);             //Disengage the Brake for Channel B
-    analogWrite(11, motorSnelheid);   //Spins the motor on Channel B at given speed
-    delay(10);                        //-----------------------DELAY?????????????-----------------
-    digitalWrite(8, HIGH);   //Engage the Brake for Channel B
+    motorSnelheid = map(klemspanning, 0, 120, 0, 90);
+    Serial.print("MotorSnelheid 0-255:");
+    Serial.println(motorSnelheid);
+    if (motorSnelheid > 40)
+      motorSnelheid = 40;
+    digitalWrite(12, LOW);           //Establishes forward direction of Channel B
+    digitalWrite(9, LOW);             //Disengage the Brake for Channel B
+    analogWrite(3, motorSnelheid);   //Spins the motor on Channel B at given speed
+    //digitalWrite(8, HIGH);   //Engage the Brake for Channel B
   }
 }
--- Testing/TestingMotor/TestingMotor.ino
+++ Testing/TestingMotor/TestingMotor.ino
@@ -1,42 +1,17 @@
-/*************************************************************
-Motor Shield 1-Channel DC Motor Demo
-by Randy Sarafan
-
-For more information see:
-https://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
-
-*************************************************************/
-
 void setup() {
   
-  //Setup Motor Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT); //Initiates Brake Channel B pin
+  //Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
   
 }
 
 void loop(){
   
   //forward @ full speed
-  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, 255);   //Spins the motor on Channel B at full speed
+  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 50);   //Spins the motor on Channel A at full speed
   
-  delay(3000);
-  
-  digitalWrite(8, HIGH); //Eengage the Brake for Channel B
-
-  delay(1000);
-  
-  //backward @ half speed
-  digitalWrite(13, LOW); //Establishes backward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, 123);   //Spins the motor on Channel B at half speed
-  
-  delay(3000);
-  
-  digitalWrite(8, HIGH); //Eengage the Brake for Channel B
-  
-  delay(1000);
   
 }
